156eb17f49d9cee9b19d896a3ccff170aca0a247
Dumbldore <mbadocha@gmail.com>
2021-09-10 16:03:50 +0200

    added more trainigs

--- run_trainings/7_SmartCommunity.sh
+++ run_trainings/7_SmartCommunity.sh
@@ -1,1 +1,1 @@
-python3 -m torch.distributed.launch --master_port 1237 --nproc_per_node  1  train.py --multi-scale  --device 4 --batch 16 --weights weights/yolov5m6.pt --data data/train/7_SmartCommunity.yaml --epochs 500 --img 640 --hyp hyp.scratch.yaml --name '7_SmartCommunity'
+python3 -m torch.distributed.launch --master_port 1237 --nproc_per_node  1  train.py --multi-scale  --device 4 --batch 12 --weights weights/yolov5m6.pt --data data/train/7_SmartCommunity.yaml --epochs 500 --img 640 --hyp hyp.scratch.yaml --name '7_SmartCommunity'
