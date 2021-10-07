0e370ddf518d9fc98c5597d5667ad422b7ff5ee4
Dumbldore <mbadocha@gmail.com>
2021-10-07 13:22:12 +0200

    hyp evolution

--- run_trainings/evolve_no_mio.sh
+++ run_trainings/evolve_no_mio.sh
@@ -1,3 +1,1 @@
-for i in 0 1 2 3 4 5 6 7; do
-  nohup python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 28 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22"  --evolve --device $i > evolve_gpu_$i.log &
-done
+python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 28 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22"  --evolve --device 0 > evolve_gpu_0.log
