af8d7df75073025a430671111086c435d8e4d78f
Dumbldore <mbadocha@gmail.com>
2021-10-13 14:19:20 +0200

    val script

--- validate_training/val_17_17.sh
+++ validate_training/val_17_17.sh
@@ -1,1 +1,1 @@
-python3 val.py --data data/train/17_SmartCommunity.yaml --weights runs/train/22_SmartCommunity/weights/best.pt --project "val_17_22"
+python3 val.py --data data/train/17_SmartCommunity.yaml --weights runs/train/17_SmartCommunity4/weights/best.pt --project "val_17_17"
