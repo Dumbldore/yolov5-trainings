098a0676ed94cbe1655811360007805cda201d72
Dumbldore <mbadocha@gmail.com>
2021-11-10 14:42:22 +0100

    new training

--- run_trainings/evolve_no_mio.sh
+++ run_trainings/evolve_no_mio.sh
@@ -1,3 +1,3 @@
 for i in 0 1 2 3 4 5 6 7; do
-  nohup python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 16 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22_working"  --evolve --device $i > evolve_gpu_$i.log &
+  nohup python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 12 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22_working"  --evolve --device $i > evolve_gpu_$i.log &
 done
