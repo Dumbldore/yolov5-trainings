96440b681df79c3f46e9f01b1e1a70b748df974a
Bartosz Bieliński <bartosz.bielinski@oke.pl>
2021-11-10 13:58:07 +0000

    merge

--- run_trainings/evolve_no_mio.sh
+++ run_trainings/evolve_no_mio.sh
@@ -1,3 +1,8 @@
 for i in 0 1 2 3 4 5 6 7; do
+<<<<<<< Updated upstream
   nohup python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 16 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22_working"  --evolve --device $i > evolve_gpu_$i.log &
 done
+=======
+  nohup python3 train.py --epochs 25 --data data/train/22_SmartCommunity_evolve.yaml --batch 12 --linear-lr --img 640 --weights weights/yolov5m.pt --name "evolve_22_smaller_batch"  --evolve --device $i > evolve_gpu_$i.log &
+done
+>>>>>>> Stashed changes
