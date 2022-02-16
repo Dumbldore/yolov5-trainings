8ba74c2ba16b90c3c90fb69ec0b4740e0a1c786b
Dumbldore <mbadocha@gmail.com>
2022-02-16 14:44:20 +0100

    new trainings

---
+++ run_trainings/v6_9.sh
@@ -0,0 +1,1 @@
+python3 train.py --device 1 --batch 28 --weights weights/yolov5m6.pt --data data/train/v6_1.yaml --epochs 150 --img 640 --optimizer "AdamW" --hyp hyp.scratch-high.yaml --exist-ok --name 'v6_3' --cfg yolov5m6.yaml
